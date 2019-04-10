0314821be24ae29689d50b52f2d245e52ccd6855
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-04-11 01:33:06 +0530

    adding material design

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule  } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatInputModule, MatCardModule, MatButtonModule } from "@angular/material";
+
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 
@@ -11,7 +14,11 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule, 
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
