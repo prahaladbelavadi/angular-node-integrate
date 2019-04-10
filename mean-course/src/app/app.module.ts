a9a3677f36673913fa2f69e441c17b8cecc9158e
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-04-11 01:55:37 +0530

    20: adding a toolbar; there is a compiler error saying it cannot find the headercomponent file, but I donnot get why, it is there;

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule  } from "@angular/forms";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatInputModule, MatCardModule, MatButtonModule } from "@angular/material";
+import { MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule } from "@angular/material";
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -18,7 +20,8 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule, 
-    MatButtonModule
+    MatButtonModule, 
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
