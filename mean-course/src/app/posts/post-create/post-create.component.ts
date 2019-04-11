6725f1dd98b42dfbe73f532fbf4bed9dbe7a8cbe
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-04-11 16:08:59 +0530

    25: adding forms; using template driven approach; static error messages

--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from "@angular/core";
 import { Post } from "../post.model";
+import { NgForm } from "@angular/forms";
 
 
 @Component({
@@ -14,10 +15,13 @@ export class PostCreateComponent {
     enteredContent = "";
     @Output() postCreated = new EventEmitter <Post>();
 
-    onAddPost(postInput: HTMLTextAreaElement){
+    onAddPost(form:NgForm){
+        if (form.invalid){
+            return;
+        }
         const post: Post = {
-            title: this.enteredTitle,
-            content: this.enteredContent
+            title: form.value.title,
+            content: form.value.content
         };
         this.postCreated.emit(post);
     }
