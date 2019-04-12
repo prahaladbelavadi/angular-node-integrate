fc07fc780885096ffda9417514f05776d0ea49b8
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-04-12 15:50:26 +0530

    add edit and deletee button ation

--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -18,6 +18,7 @@ export class PostCreateComponent {
         if (form.invalid){
             return;
         }
-        this.postsService.addPost(form.value.title, form.value.content)
+        this.postsService.addPost(form.value.title, form.value.content);
+        form.resetForm();
     }
 }
