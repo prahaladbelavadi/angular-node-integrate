a26a6b3155b31d3905bf231103d10001ee7d265f
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-04-14 00:43:55 +0530

    40: add post req and a body parser

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 
 const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}))
 
 app.use((req,res, next)=>{
     
@@ -13,7 +18,15 @@ app.use((req,res, next)=>{
 })
 
 
-app.use('/api/posts' ,(req,res,next)=>{
+app.post("/api/posts", (req,res, next)=>{
+    const posts = req.body; 
+    console.log(post);
+    res.status(201).json({
+        message: 'Post added Successfully'
+    });
+})
+
+app.get('/api/posts' ,(req,res,next)=>{
     
     const posts = [
       {
