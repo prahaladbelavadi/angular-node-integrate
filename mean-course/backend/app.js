f298a1da6b0ef77cc25af73ab8b4bcb2fa6375ac
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-04-14 01:00:00 +0530

    correct cors headers and fix a typo

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use((req,res, next)=>{
     
     res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Header',"Origin, X-Requested-With,Content-Type, Accept");
+    res.setHeader('Access-Control-Allow-Headers',"Origin, X-Requested-With,Content-Type, Accept");
 
     res.setHeader("Access-Control-Allow-Methods", "GET, POST,PATCH,DELETE, OPTIONS");
     
@@ -20,7 +20,7 @@ app.use((req,res, next)=>{
 
 app.post("/api/posts", (req,res, next)=>{
     const posts = req.body; 
-    console.log(post);
+    console.log(posts);
     res.status(201).json({
         message: 'Post added Successfully'
     });
--- mean-course/src/app/posts/posts.service.ts
+++ mean-course/src/app/posts/posts.service.ts
@@ -1,34 +1,40 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import { Subject } from "rxjs";
 
-import { Post } from './post.model';
-import { HttpClient } from '@angular/common/http';
+import { Post } from "./post.model";
+import { HttpClient } from "@angular/common/http";
 
+@Injectable({ providedIn: "root" })
+export class PostsService {
+  private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
 
+  constructor(private http: HttpClient) {}
 
-@Injectable({ providedIn: 'root' })
-export class PostsService{
-    private posts: Post[] = [];
-    private postsUpdated = new Subject<Post[]>();
+  getPosts() {
+    this.http
+      .get<{ message: string; posts: Post[] }>(
+        "http:///localhost:3000/api/posts"
+      )
+      .subscribe(postData => {
+        this.posts = postData.posts;
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
 
-    constructor(private http:HttpClient){}
+  getPostUpdateListener() {
+    return this.postsUpdated.asObservable();
+  }
 
-    getPosts(){
-        this.http.get<{message: string, posts: Post[]}>('http:///localhost:3000/api/posts')
-        .subscribe((postData)=>{
-            this.posts = postData.posts;
-            this.postsUpdated.next([...this.posts]);
-        });
-    }
+  addPost(title: string, content: string) {
+    const post: Post = { id: null, title: title, content: content };
+    this.http
+      .post<{ message: string }>("http://localhost:3000/api/posts", post)
+      .subscribe(responseData => {
+        console.log(responseData.message);
 
-    getPostUpdateListener(){
-        return this.postsUpdated.asObservable()
-    }
-
-    addPost(title:string, content: string){
-        const post: Post = {id: null, title: title, content: content}
-
-        this.posts.push(post)
-        this.postsUpdated.next([...this.posts])
-    }
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
 }
