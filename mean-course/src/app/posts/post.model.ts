e60bca61f1505732dd881498d9d8ba2a92ef39b9
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-04-14 00:10:23 +0530

    38: making get request with cors; it doesnot work due to cors

--- mean-course/src/app/posts/post.model.ts
+++ mean-course/src/app/posts/post.model.ts
@@ -1,4 +1,5 @@
 export interface Post {
     title: string;
     content: string;
+    id: string;
 }
--- mean-course/src/app/posts/posts.service.ts
+++ mean-course/src/app/posts/posts.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Subject } from "rxjs";
 
 import { Post } from './post.model';
+import { HttpClient } from '@angular/common/http';
 
 
 
@@ -10,8 +11,14 @@ export class PostsService{
     private posts: Post[] = [];
     private postsUpdated = new Subject<Post[]>();
 
+    constructor(private http:HttpClient){}
+
     getPosts(){
-        return [...this.posts];
+        this.http.get<{message: string, posts: Post[]}>('http:///localhost:3000/api/posts')
+        .subscribe((postData)=>{
+            this.posts = postData.posts;
+            this.postsUpdated.next([...this.posts]);
+        });
     }
 
     getPostUpdateListener(){
@@ -19,7 +26,7 @@ export class PostsService{
     }
 
     addPost(title:string, content: string){
-        const post: Post = {title: title, content: content}
+        const post: Post = {id: null, title: title, content: content}
 
         this.posts.push(post)
         this.postsUpdated.next([...this.posts])
