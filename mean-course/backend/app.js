880b1745678a469845e048ef69f9d5d58ee07a4b
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-04-15 00:44:44 +0530

    deleting documents: 57; the front end doesnot update after sending delete request to backend

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -59,4 +59,13 @@ app.get("/api/posts", (req, res, next) => {
 
 });
 
+app.delete("/api/posts/:id", (rew,res, next)=>{
+  Post.deleteOne({ _id: rew.params.id }).then(result => {
+    console.log(result)
+
+  res.status(200).json({ message: "Post Deleted!" });
+
+  });
+})
+
 module.exports = app;
