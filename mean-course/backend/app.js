c817c2b8b294a6f876290331e2d38f9ef37543f6
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-04-14 22:31:32 +0530

    54: fetching data from a database

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -1,66 +1,63 @@
-const express = require('express');
+const express = require("express");
 const bodyParser = require("body-parser");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Post  = require('./models/post');
+const Post = require("./models/post");
 
 const app = express();
 
-mongoose.connect("mongodb+srv://admin:3LMVtL3qs1sJRreU@cluster0-njshn.mongodb.net/node-angular?retryWrites=true")
-.then(()=>{
+mongoose
+  .connect(
+    "mongodb+srv://admin:3LMVtL3qs1sJRreU@cluster0-njshn.mongodb.net/node-angular?retryWrites=true"
+  )
+  .then(() => {
     console.log("Connected to database");
-})
-.catch(()=>{
-    console.log("Connection failed")
-});
+  })
+  .catch(() => {
+    console.log("Connection failed");
+  });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req,res, next)=>{
-    
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Headers',"Origin, X-Requested-With,Content-Type, Accept");
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With,Content-Type, Accept"
+  );
 
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST,PATCH,DELETE, OPTIONS");
-    
-    next()
-})
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST,PATCH,DELETE, OPTIONS"
+  );
+
+  next();
+});
+
+app.post("/api/posts", (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  post.save();
+
+  res.status(201).json({
+    message: "Post added Successfully"
+  });
+});
+
+app.get("/api/posts", (req, res, next) => {
+  Post.find().then(documents => {
+        res.status(200).json({
+        message: "Posts fetched Successfully",
+        posts: documents
+        });
+  });
 
 
-app.post("/api/posts", (req,res, next)=>{
-    const post = new Post({
-        title: req.body.title,
-        content: req.body.content
-    });
+});
 
-    post.save();
-
-    res.status(201).json({
-        message: 'Post added Successfully'
-    });
-})
-
-app.get('/api/posts' ,(req,res,next)=>{
-    
-    const posts = [
-      {
-        title: "First server side spost",
-        content: "fromt the dark side",
-        id: "wfkjnwkjfn"
-      },
-      {
-        title: "Second server side spost",
-        content: "fromt the dark side",
-        id: "wfkjnwkjfn"
-      }
-    ];
-    res.status(200).json(
-        {
-            message:'Posts fetched Successfully',
-        posts: posts
-})
-})
-
-module.exports = app
+module.exports = app;
