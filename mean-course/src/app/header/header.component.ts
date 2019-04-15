1893404fb844d8b0d8d853b7ed1df94461f04e36
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-04-15 14:31:22 +0530

    add styling visual feedback stuff: 62;  active linnk

--- mean-course/src/app/header/header.component.ts
+++ mean-course/src/app/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector:'app-header',
     templateUrl:'./header.component.html',
-    styleUrls:[],
+    styleUrls: ['./header.component.css'],
 
 })
 
