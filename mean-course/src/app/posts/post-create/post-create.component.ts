47f1115b53dc34c9d0482ab06cf98876787b8905
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-04-16 16:51:16 +0530

    add image preview

--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -16,6 +16,7 @@ export class PostCreateComponent implements OnInit {
     post: Post;
     isLoading = false;
     form:FormGroup
+    imagePreview : any;
 
     private mode = "create";
     private postId: string;
@@ -62,8 +63,12 @@ export class PostCreateComponent implements OnInit {
         const file = (event.target as HTMLInputElement ).files[0];
         this.form.patchValue({image:file});
         this.form.get('image').updateValueAndValidity();
-        console.log(file);
-        console.log(this.form);
+
+        const reader = new FileReader();
+        reader.onload = () => {
+            this.imagePreview = reader.result;
+        };
+        reader.readAsDataURL(file);
     }
 
 
