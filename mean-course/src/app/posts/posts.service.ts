dc9630be1c82b51a0dbb406178a4dd2f7608700f
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-04-16 15:16:11 +0530

    correct service

--- mean-course/src/app/posts/posts.service.ts
+++ mean-course/src/app/posts/posts.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 import { Subject } from "rxjs";
 import { map } from "rxjs/operators";
-import { HttpClient } from "@angular/common/http";
-import { Router } from '@angular/router';
+import { Router } from "@angular/router";
 
 import { Post } from "./post.model";
 
@@ -11,22 +11,22 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
-  constructor(private http: HttpClient,private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   getPosts() {
     this.http
-      .get<{ message: string; posts: any }>(
-        "http:///localhost:3000/api/posts"
-      )
-      .pipe(map((postData) => {
-        return postData.posts.map(post => {
-          return {
-            title: post.title,
-            content: post.content,
-            id: post._id
-          }
+      .get<{ message: string; posts: any }>("http://localhost:3000/api/posts")
+      .pipe(
+        map(postData => {
+          return postData.posts.map(post => {
+            return {
+              title: post.title,
+              content: post.content,
+              id: post._id
+            };
+          });
         })
-      }))
+      )
       .subscribe(transformedPosts => {
         this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
@@ -37,52 +37,49 @@ export class PostsService {
     return this.postsUpdated.asObservable();
   }
 
-  getPost(id: string){
-    // return {...this.posts.find(p => p.id === id)}
-    return this.http.get<{ _id:string, title:string, content: string }>("http://localhost:3000api/posts/"+id)
+  getPost(id: string) {
+    return this.http.get<{ _id: string; title: string; content: string }>(
+      "http://localhost:3000/api/posts/" + id
+    );
   }
 
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
     this.http
-      .post<{ message: string, postId: string }>("http://localhost:3000/api/posts", post)
+      .post<{ message: string; postId: string }>(
+        "http://localhost:3000/api/posts",
+        post
+      )
       .subscribe(responseData => {
         const id = responseData.postId;
         post.id = id;
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
-
         this.router.navigate(["/"]);
       });
   }
 
-  updatePost(id: string, title:string, content: string){
-    const post: Post = {
-      id: id,
-      title: title,
-      content: content
-    }
-
-    this.http.put("http://localhost:3000/api/posts/" + id, post).subscribe(response=>{
-    const updatedPosts = [...this.posts];
-    const oldPostIndex = updatedPosts.findIndex(p=> p.id ===post.id);
-
-    updatedPosts[oldPostIndex] = post;
-    this.posts = updatedPosts
-    this.postsUpdated.next([...this.posts]);
-    this.router.navigate(["/"])
-
-    })
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = { id: id, title: title, content: content };
+    this.http
+      .put("http://localhost:3000/api/posts/" + id, post)
+      .subscribe(response => {
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+        this.router.navigate(["/"]);
+      });
   }
 
   deletePost(postId: string) {
-    this.http.delete("http://localhost:3000/api/posts/" + postId)
-      .subscribe(() => {
+    this.http
+      .delete("http://localhost:3000/api/posts/" + postId)
+      .subscribe(() => {  
         const updatedPosts = this.posts.filter(post => post.id !== postId);
         this.posts = updatedPosts;
-        this.postsUpdated.next([...this.posts])
-      })
+        this.postsUpdated.next([...this.posts]);
+      });
   }
-
-
 }
