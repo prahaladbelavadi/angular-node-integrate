1499eea13a5aa11908a006e0b71b725d7ff04c9d
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-04-18 13:38:05 +0530

    Revert "83: updating posts with images"
    This reverts commit 193da42e7b758a1beef6fd6b5f56c10a674046b4.

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -1,4 +1,3 @@
-var path =  require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -20,8 +19,6 @@ mongoose
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
