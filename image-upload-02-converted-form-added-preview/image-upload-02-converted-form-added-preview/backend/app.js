6b3d69c5cec1ab28cc37b2777cd61da7c20d0954
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-04-18 14:25:04 +0530

    added to check; will delete later

---
+++ image-upload-02-converted-form-added-preview/image-upload-02-converted-form-added-preview/backend/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const postsRoutes = require("./routes/posts");
+
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:3LMVtL3qs1sJRreU@cluster0-njshn.mongodb.net/node-angular?retryWrites=true"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/posts", postsRoutes);
+
+module.exports = app;
---
+++ image-upload-02-converted-form-added-preview/image-upload-02-converted-form-added-preview/src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import {
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatExpansionModule,
+  MatProgressSpinnerModule
+} from "@angular/material";
+
+import { AppComponent } from "./app.component";
+import { PostCreateComponent } from "./posts/post-create/post-create.component";
+import { HeaderComponent } from "./header/header.component";
+import { PostListComponent } from "./posts/post-list/post-list.component";
+import { AppRoutingModule } from "./app-routing.module";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
