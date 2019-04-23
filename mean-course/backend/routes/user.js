9a3f7572a1502216a825b24da6af17422bd30a61
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-04-24 02:00:50 +0530

    101: add spa authentication using jwt ;

--- mean-course/backend/routes/user.js
+++ mean-course/backend/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const User = require("../models/user")
+const jwt = require("jsonwebtoken")
 
 const router = express.Router();
 
@@ -28,6 +29,39 @@ router.post("/signup", (req, res, next) => {
         })
 
 })
+
+router.post("/login", (req, res, next) => {
+    User.find({
+        email: req.body.email
+    })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({
+                    message: "Auth Failed"
+                });
+            } else {
+                return bcrypt.compare(req.body.password, user.password);
+            }
+        })
+        .then(result => {
+            if (!result) {
+                return res.status(401).json({
+                    message: "Auth Failed"
+                });
+            }
+
+            const token = jwt.sign(
+                { email: user.email, userId: user._id },
+                "Secret_key",
+                { expiresIn: "1h" }
+            );
+        })
+        .catch(err => {
+            return res.status(401).json({
+                message: "Auth Failed"
+            });
+        });
+});
 
 
 module.exports = router;
