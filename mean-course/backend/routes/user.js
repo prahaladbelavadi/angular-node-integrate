00955836e79f701ea8f45c0385aef74f491b61a8
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-04-24 02:36:27 +0530

    102: sending auth token to front end and signing em in; error: responds with a 401; somewhere from backend end it is trpping gotta figure it out

--- mean-course/backend/routes/user.js
+++ mean-course/backend/routes/user.js
@@ -15,33 +15,38 @@ router.post("/signup", (req, res, next) => {
             });
 
             user.save()
-                .then((result)=>{
+                .then((result) => {
                     res.status(201).json({
                         message: 'User Created!',
                         result: result
                     });
-            })
-            .catch(err =>{
-                res.status(500).json({
-                    error:err
                 })
-            });
+                .catch(err => {
+                    res.status(500).json({
+                        error: err
+                    })
+                });
         })
 
 })
 
 router.post("/login", (req, res, next) => {
+
+let fetchedUser;
+
     User.find({
         email: req.body.email
     })
         .then(user => {
+            // console.log(user)
             if (!user) {
                 return res.status(401).json({
                     message: "Auth Failed"
                 });
-            } else {
-                return bcrypt.compare(req.body.password, user.password);
             }
+                
+            fetchedUser = user;
+            return bcrypt.compare(req.body.password, user.password);
         })
         .then(result => {
             if (!result) {
@@ -51,17 +56,23 @@ router.post("/login", (req, res, next) => {
             }
 
             const token = jwt.sign(
-                { email: user.email, userId: user._id },
+                { email: fetchedUser.email, userId: fetchedUser._id },
                 "Secret_key",
                 { expiresIn: "1h" }
             );
+
+            res.status(200).json({
+                token: token
+            })
         })
-        .catch(err => {
+        .catch((err) => {
             return res.status(401).json({
                 message: "Auth Failed"
             });
         });
 });
 
+
+router.post("")
 
 module.exports = router;
--- mean-course/src/app/auth/login/login.component.ts
+++ mean-course/src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { AuthService } from "../auth.service";
 
 @Component({
     templateUrl:'./login.component.html',
@@ -10,8 +11,14 @@ export class LoginComponent{
 
     isLoading = false;
 
+    constructor(public authService: AuthService){}
+
     onLogin(form:NgForm){
-        console.log(form.value)
+        if(form.invalid){
+            return
+        }
+        this.authService.login(form.value.email, form.value.password);
+
     }
 
 
