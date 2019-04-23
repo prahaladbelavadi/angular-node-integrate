a1f473698c94c6772c7435ab220c1029d353eea8
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-04-23 18:09:52 +0530

    97: creating user model; add mmongose unique validator

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -5,6 +5,9 @@ const mongoose = require("mongoose");
 
 const postsRoutes = require("./routes/posts");
 
+const userRoutes = require("./routes/user")
+
+
 const app = express();
 
 mongoose
@@ -36,5 +39,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/posts", postsRoutes);
+app.use("/api/user", userRoutes);
+
 
 module.exports = app;
