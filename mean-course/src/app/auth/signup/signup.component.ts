735af448e345d4c6657de56b85d7ccfc4bdc9c46
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-04-23 17:47:58 +0530

    add signup screen:96

---
+++ mean-course/src/app/auth/signup/signup.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+@Component({
+    templateUrl:'./signup.component.html',
+    styleUrls: ['./signup.component.css']
+})
+
+export class SignupComponent{
+
+    isLoading = false;
+
+    onSignup(form:NgForm){
+        console.log(form.value)
+    }
+
+
+
+}
