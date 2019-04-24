832d19eb1fab5214ddffa21fe4adeaed804d1c2d
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-04-24 16:46:49 +0530

    105: improving header component to reflect authentication status

--- mean-course/src/app/auth/auth.service.ts
+++ mean-course/src/app/auth/auth.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { AuthData } from "./auth-data.model";
+import { Subject } from "rxjs";
 
 @Injectable({
     providedIn: "root"
 })
 export class AuthService {
     private token: string;
+    private authStatusListener = new Subject<boolean>();
 
     constructor(private http: HttpClient) { }
 
     getToken(){
         return this.token;
+    }
+
+    getAuthStatusListener(){
+        return this.authStatusListener.asObservable();
     }
 
     createUser(email: string, password: string) {
@@ -32,6 +38,7 @@ export class AuthService {
         .subscribe((response)=>{
             const token = response.token;
             this.token = token;
+            this.authStatusListener.next(true)
         })
     }
 
