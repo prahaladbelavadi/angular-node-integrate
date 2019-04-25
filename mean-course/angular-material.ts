524fafd7d2f8596aa71b4fcf758cf01367f6889d
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-04-26 01:40:12 +0530

    creating an angular Material module: 131

---
+++ mean-course/angular-material.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import {
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatDialogModule
+} from '@angular/material';
+
+@NgModule({
+     exports:[
+         MatInputModule,
+         MatCardModule,
+         MatButtonModule,
+         MatToolbarModule,
+         MatExpansionModule,
+         MatProgressSpinnerModule,
+         MatPaginatorModule,
+         MatDialogModule,
+     ]
+})
+
+export class AngularMaterialModule{}
--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -1,29 +1,21 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { NgModule } from "@angular/core";
-import { ReactiveFormsModule, FormsModule } from "@angular/forms";
-import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
-import {
-  MatInputModule,
-  MatCardModule,
-  MatButtonModule,
-  MatToolbarModule,
-  MatExpansionModule,
-  MatProgressSpinnerModule,
-  MatPaginatorModule,
-  MatDialogModule
-} from "@angular/material";
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
-import { AppComponent } from "./app.component";
-import { PostCreateComponent } from "./posts/post-create/post-create.component";
-import { HeaderComponent } from "./header/header.component";
-import { PostListComponent } from "./posts/post-list/post-list.component";
-import { AppRoutingModule } from "./app-routing.module";
-import { LoginComponent } from "./auth/login/login.component";
-import { SignupComponent } from "./auth/signup/signup.component";
-import { AuthInterceptor } from "./auth/auth-interceptor";
-import { ErrorInterceptor } from "./error-interceptor";
-import { ErrorComponent } from "./error/error.component";
+
+import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { AuthInterceptor } from './auth/auth-interceptor';
+import { ErrorInterceptor } from './error-interceptor';
+import { ErrorComponent } from './error/error.component';
+import { AngularMaterialModule } from 'angular-material';
 
 @NgModule({
   declarations: [
@@ -41,15 +33,8 @@ import { ErrorComponent } from "./error/error.component";
     ReactiveFormsModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatExpansionModule,
-    MatProgressSpinnerModule,
-    MatPaginatorModule,
-    MatDialogModule,
-    HttpClientModule
+    HttpClientModule,
+    AngularMaterialModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
