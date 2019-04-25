707c46b826d859949c31e0c3625588b0dba982f6
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-04-25 12:31:21 +0530

    add user id to posts to make note of who created the post: 115

--- mean-course/backend/middleware/check-auth.js
+++ mean-course/backend/middleware/check-auth.js
@@ -3,7 +3,8 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
     try{
         const token = req.headers.authorization.split(" ")[1];
-        jwt.verify(token, "Secret_key")
+       const decodedToken =  jwt.verify(token, "Secret_key");
+       req.userData = {email: decodedToken.email, userId: decodedToken.userId }
         next()
     } catch(error){
         res.status(401).json({message: "aauth failed!"})
